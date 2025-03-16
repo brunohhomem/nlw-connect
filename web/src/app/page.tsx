37e48021258843d5de176c0f6b123fe947277a59
Bruno H. Homem <brunohhomem@gmail.com>
2025-03-16 08:02:53 -0300

    feat: add input component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/button'
 import { IconButton } from '@/components/icon-button'
-import { ArrowRight, Copy } from 'lucide-react'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -12,6 +13,15 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+          <InputField></InputField>
+        </InputRoot>
+      </div>
     </main>
   )
 }
