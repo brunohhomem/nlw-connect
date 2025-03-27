12e50af8a7f8d0f39d580852af3d720e755a4a10
Bruno H. Homem <brunohhomem@gmail.com>
2025-03-27 05:28:53 -0300

    feat: invite page

--- web/src/app/invite/page.tsx
+++ web/src/app/invite/page.tsx
@@ -4,6 +4,9 @@ import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import logo from '../../assets/logo.svg'
+import cooper from '../../assets/medal-cooper.svg'
+import gold from '../../assets/medal-gold.svg'
+import silver from '../../assets/medal-silver.svg'
 
 export default function InvitePage() {
   return (
@@ -78,6 +81,44 @@ export default function InvitePage() {
 
               <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1°</span> | Vit Homem
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              100
+            </span>
+            <Image src={gold} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1°</span> | Butters Homem
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              99
+            </span>
+            <Image src={silver} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1°</span> | Bruno Homem
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              55
+            </span>
+            <Image src={cooper} alt="" className="absolute top-0 right-8" />
           </div>
         </div>
       </div>
